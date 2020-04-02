8f70f612e0120479c7a68255d68f79c23208e23e
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-02 17:16:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key) {

417d39c948e26e03a0a2f3c96b6022309d6a46e5
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-02 18:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj
 }
 
-function destructivelyDeleteObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
